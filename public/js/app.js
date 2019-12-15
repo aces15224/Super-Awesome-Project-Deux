5856a142b4776cc4bd67b7606f80aac8e674c170
aces15224 <53838763+aces15224@users.noreply.github.com>
2019-12-14 19:37:27 -0600

    Zip Code Function and Validation for Buyer Page
    !

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,36 @@
 // Buyer Search Button & Ajax Call
 
 $("#searchButton").on("click", function(event){
-    event.preventDefault();
-    var zipcode = $("#areaSelect").val().trim();
-    var radius = $("#zipRadius").val()
-    var queryURL="https://api.zip-codes.com/ZipCodesAPI.svc/1.0/FindZipCodesInRadius?zipcode=" + zipcode + "&minimumradius=0&maximumradius=" + radius + "&key=OTXG2RB5WPBTU3O8BZEA";
- $.ajax({
-   url: queryURL,
-   method: "GET"
- }).then(function(response) {
-    for(i=0; i<response.DataList.length; i++){
-      console.log(response.DataList[i].Code)
-  }});  
+  event.preventDefault();
+  var min = $("#minPrice").val();
+  var max = $("#maxPrice").val();
+  var zipcode = $("#areaSelect").val().trim();
+  var radius = $("#zipRadius").val()
+  var bed = $("#bedNum").val()
+  
+  if(min >= 0 && max >= 1 && zipcode === "" && radius === "" && bed === ""){
+    alert("Yes")
+  }
+  
+  if(min === "" && max === "" && zipcode === "" && radius === "" && bed >= 1){
+    alert("YAR")
+  }
+
+  if(min === "" && max === "" && zipcode.length === 5 && radius != "" && bed === ""){
+    zipCode(zipcode, radius)
+  }    
 }); 
+
+// ZipCode Function //
+
+function zipCode(zipcode, radius){
+  var queryURL="https://api.zip-codes.com/ZipCodesAPI.svc/1.0/FindZipCodesInRadius?zipcode=" + zipcode + "&minimumradius=0&maximumradius=" + radius + "&key=OTXG2RB5WPBTU3O8BZEA";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+      for(i=0; i<response.DataList.length; i++){
+        console.log(response.DataList[i].Code)
+  }});  
+} 
+ 
