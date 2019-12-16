6bcb3a371b3c165db962d1f51b0efb5ba30eaf8f
aces15224 <53838763+aces15224@users.noreply.github.com>
2019-12-15 22:07:01 -0600

    Update
    Working switch case, functional filter, and radio button validation.  Character validation still required.  80% done.

--- public/js/app.js
+++ public/js/app.js
@@ -1,36 +1,56 @@
-// Buyer Search Button & Ajax Call
+// Search and Filter Functions//
 
 $("#searchButton").on("click", function(event){
   event.preventDefault();
-  var min = $("#minPrice").val();
-  var max = $("#maxPrice").val();
-  var zipcode = $("#areaSelect").val().trim();
-  var radius = $("#zipRadius").val()
-  var bed = $("#bedNum").val()
-  
-  if(min >= 0 && max >= 1 && zipcode === "" && radius === "" && bed === ""){
-    alert("Yes")
+
+  if($('input[name="filter"]:checked').val()){
+    var switchVal=$('input[name="filter"]:checked').val();
+      switch (switchVal){
+        case "one":
+          var min = $("#minPrice").val();
+          var max = $("#maxPrice").val();
+          if(min >= 0 && max >=1){
+            priceMatch(min,max)
+            break;
+          }
+          else{
+            return false;
+          }
+        case "two":
+          var zipcode = $("#areaSelect").val().trim();
+          var radius = $("#zipRadius").val();
+          if(radius!="" && zipcode.length===5)
+            areaCode(zipcode, radius)
+            break;
+
+        case "three":
+          var bed = $("#bedNum").val();
+          if(bed >= 1){
+            bedrooms(bed)
+            break; 
+          }
+          return false;
+      }
   }
-  
-  if(min === "" && max === "" && zipcode === "" && radius === "" && bed >= 1){
-    alert("YAR")
-  }
 
-  if(min === "" && max === "" && zipcode.length === 5 && radius != "" && bed === ""){
-    zipCode(zipcode, radius)
-  }    
-}); 
+function priceMatch(min, max){
+  console.log(min)
+  console.log(max)
+}
 
-// ZipCode Function //
+function bedrooms(bed){
+  console.log(bed)
+}
 
-function zipCode(zipcode, radius){
+function areaCode(zipcode, radius){
   var queryURL="https://api.zip-codes.com/ZipCodesAPI.svc/1.0/FindZipCodesInRadius?zipcode=" + zipcode + "&minimumradius=0&maximumradius=" + radius + "&key=OTXG2RB5WPBTU3O8BZEA";
-  $.ajax({
+    $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
+    }).then(function(response) {
       for(i=0; i<response.DataList.length; i++){
         console.log(response.DataList[i].Code)
-  }});  
-} 
+    }});  
+}}); 
+
  
