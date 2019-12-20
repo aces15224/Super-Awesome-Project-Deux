6894064b71a2eaa9ceb3ee69001ce0e68910a20f
aces15224 <53838763+aces15224@users.noreply.github.com>
2019-12-20 08:02:20 -0600

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
 // Dependencies
 var Sequelize = require("sequelize");
+var mysql = require("mysql");
+var connection;
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
 var sequelize = new Sequelize("Listings_db", "root", "password", {
@@ -13,29 +15,25 @@ var sequelize = new Sequelize("Listings_db", "root", "password", {
   }
 });
 
-// Exports the connection for other files to use
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "test",
+    password: "password",
+    database: "listings_db"
+  });
+}
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
 module.exports = sequelize;
-
-
-
-// var mySQL=require("mysql");
-
-// var connection=mySQL.createConnection({
-//     host:"localhost",
-//     user:"root",
-//     password:"password",
-//     port:3306,
-//     database: "example_db"
-// })
-
-// connection.connect(function(err){
-//     if (err){
-//         console.error("error connecting: " + err.stack);
-//         return;
-//     }
-//     console.log("connected as id " + connection.threadId)
-// })
-
-
-
-// module.exports=connection;
