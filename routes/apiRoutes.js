6a6476fa8e7b4be563f33e7af9a137389edac8d6
aces15224 <53838763+aces15224@users.noreply.github.com>
2019-12-20 21:09:52 -0600

    api routes for bedroom search
     Where function is not working quite yet.  Need to figure out a way to collect input values and reference them with the ajax call.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,61 @@
 var db = require("../models");
+// var bedrooms = require("../public/js/app.js")
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/new", function(req, res) {
-    db.Listings.findAll({}).then(function(dbListingss) {
-      res.json(dbListingss);
+  // Get all Listings
+  app.get("/listings", function(req, res) {
+    db.Listings.findAll({}).then(function(dbListings) {
+      res.json(dbListings);
     });
   });
 
+  app.get("/listings/bedrooms/:bedrooms?", function(req, res) {
+    db.Listings.findAll({
+      where:{
+        bedrooms:req.params.bedrooms
+      }
+    }).then(function(dbListings) {
+        res.json(dbListings);
+      });
+    });
+
+
+  // app.get("/listings/:price?", function(req, res) {
+  //   db.Listings.findAll({
+  //     where: {
+  //       sellingPrice:{
+  //         $between: [50000, 200000]
+  //       } 
+  //     }
+  //   }).then(function(dbListings) {
+  //     console.log(dbListings)
+  //     res.json(dbListings);
+  //   });
+  // });
+
+
+// get all zipcodes 
+
+  // app.get("/listings/zipcode", function(req, res) {
+  //   db.Listings.findAll({
+  //     include:[{
+  //       model:Listings,
+  //       as
+  //     }]
+  //   }).then(function(dbListings) {
+  //     res.json(dbListings);
+  //   });
+  // });
+
   // Create a new Listings
-  app.post("/api/new", function(req, res) {
+  app.post("/listings", function(req, res) {
     db.Listings.create(req.body).then(function(dbListings) {
       res.json(dbListings);
     });
   });
 
   // Delete an Listings by id
-  app.delete("/api/new/:id", function(req, res) {
+  app.delete("/listings:id", function(req, res) {
     db.Listings.destroy({ where: { id: req.params.id } }).then(function(dbListings) {
       res.json(dbListings);
     });
