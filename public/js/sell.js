4e4f8b949b80ed6a7c2509acb28f9e9224bafb71
aces15224 <53838763+aces15224@users.noreply.github.com>
2019-12-25 13:20:23 -0600

    Zip Code Validation
    To ensure zipcodes collected by form are in the NKC area.

--- public/js/sell.js
+++ public/js/sell.js
@@ -2,29 +2,40 @@
 
 $("#sellButton").on("click", function(event){
   event.preventDefault();
+  var zipCodeArray = [64116, 64106, 64124, 64105, 64123, 64115, 64117, 64120, 64121, 64127, 64101, 64108, 64102, 66101]
+  var zipcode = $("#areaZip").val().trim();
+  var zipParse = parseInt(zipcode)
 
-  var newListing = {
-    sellerName: $("#name").val(),
-    email: $("#email").val(),
-    sellingPrice: $("#price").val(),
-    sqFootage: $("#sqFoot").val(),
-    bedrooms: $("#bedrooms").val(),
-    areaZip: $("#areaZip").val(),
-    image: $("#image").val(),
-    hotAndCold:$("#hotAndCold").val()
-  };
+  for (i = 0; i < zipCodeArray.length; i++) {
+    var zip = zipCodeArray[i];
+    if (zipParse === zip) {
+      alert("yes")
+      var newListing = {
+        sellerName: $("#name").val(),
+        email: $("#email").val(),
+        sellingPrice: $("#price").val(),
+        sqFootage: $("#sqFoot").val(),
+        bedrooms: $("#bedrooms").val(),
+        areaZip: $("#areaZip").val(),
+        image: $("#image").val(),
+        hotAndCold:$("#hotAndCold").val()
+    };
+  console.log(newListing)
 
-  $.post("/listings", newListing)
-    .then(function(data) {
-    console.log(data);
-    });
+      $.post("/listings", newListing)
+        .then(function(data) {
+        console.log(data);
+        });
 
-  $("#name").val("");
-  $("#email").val("");
-  $("#price").val("");
-  $("#sqFoot").val("");
-  $("#bedrooms").val("");
-  $("#areaZip").val("");
-  $("#image").val("");
-  $("#hotAndCold").val("")
+      $("#name").val("");
+      $("#email").val("");
+      $("#price").val("");
+      $("#sqFoot").val("");
+      $("#bedrooms").val("");
+      $("#areaZip").val("");
+      $("#image").val("");
+      $("#hotAndCold").val("")
+    }
+    return false;
+    }
 });
