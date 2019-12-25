29a26731ed043e7cfb9119f205e40872c9608b8f
aces15224 <53838763+aces15224@users.noreply.github.com>
2019-12-25 17:21:02 -0600

    School Information
    Array of objects.  Easier than creating routes for the schooling information.

---
+++ data/school.js
@@ -0,0 +1,35 @@
+var schools=[
+{
+    schoolName:"Chapel Hill Elementary School", zipCode: 64119, rating: 10},
+    {schoolName:"Nashua Elementary School", zipCode: 64155, rating: 10},
+    {schoolName:"West Englewood Elementary School", zipCode: 64118, rating: 9},
+    {schoolName:"Fox Hill Elementary School", zipCode: 64155, rating: 9},
+    {schoolName:"Staley High School", zipCode: 64156, rating: 9},
+    {schoolName:"Bell Prairie Elementary School", zipCode: 64156,rating: 9},
+    {schoolName:"Clardy Elementary School", zipCode: 64118, rating: 8},
+    {schoolName:"Crestview Elementary School", zipCode: 64116, rating: 8},
+    {schoolName:"Davidson Elementary School", zipCode: 64118, rating: 8},
+    {schoolName:"New Mark Middle School", zipCode: 64155, rating: 8},
+    {schoolName:"Oak Park High School", zipCode: 64118, rating: 8},
+    {schoolName:"Topping Elementary School", zipCode: 64117, rating: 8},
+    {schoolName:"Northview Elementary School", zipCode: 64156, rating: 8},
+    {schoolName:"Antioch Middle School", zipCode: 64118, rating: 7},
+    {schoolName:"Gracemor Elementary School", zipCode: 64119, rating: 7},
+    {schoolName:"Linden West Elementary School", zipCode: 64118, rating: 7},
+    {schoolName:"Gashland Elementary School", zipCode: 64118, rating: 8},
+    {schoolName:"Maple Park Middle School", zipCode: 64119, rating: 4},
+    {schoolName:"Winnwood Elementary School", zipCode: 64117, rating: 5},
+    {schoolName:"Winnetonka High School", zipCode: 64119, rating: 5},
+    {schoolName:"Eastgate Middle School", zipCode: 64117, rating: 5},
+    {schoolName:"Chouteau Elementary School", zipCode: 64117, rating: 5},
+    {schoolName:"Briarcliff Elementary School", zipCode: 64116, rating: 5},
+    {schoolName:"Northgate Middle School", zipCode: 64118, rating:  6},
+    {schoolName:"Lakewood Elementary School", zipCode: 64117, rating: 6},
+    {schoolName:"Ravenwood Elementary School", zipCode: 64119, rating: 7},
+    {schoolName:"Oakwood Manor Elementary School", zipCode: 64118, rating: 7},
+    {schoolName:"North Kansas City High School", zipCode: 64116, rating: 7},
+    {schoolName:"Meadowbrook Elementary School", zipCode: 64118, rating: 7},
+    {schoolName:"Maplewood Elementary School", zipCode: 64119, rating: 7}
+];
+
+    module.exports=schools;
